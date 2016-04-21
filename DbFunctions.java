de46a7edbe849442c2740ee312c96e369febd83b
Raj Jogi <rajjogi1994@gmail.com>
2016-04-21 15:12:16 -0400

    Database accessing functions
    Database functions for returning things stored in db

---
+++ DbFunctions.java
@@ -0,0 +1,37 @@
+package pojos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbFunctions {
+	//class for accessing db
+	
+	public DbFunctions(String dbname, String dbhost, String pass) 
+	{
+		//connect to DB (should be 
+		try {
+			Connection conn = DriverManager.getConnection("jdbc://mysql://localhost/Oconee", dbhost, pass);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void readAllFireFighters() 
+	{
+		String query = "SELECT * FROM Firefighters";
+	}
+	
+	public void readExpired(boolean param)
+	{
+		
+		String query = "SELECT * FROM FireFighters WHERE ";
+		
+		//PreparedStatement ps = connection.PreparedStatement(query);
+	
+	}
+
+}
