e4e980c46239ce5c7ec92229f0c7cbb6b607a36a
elewis13 <elewis13@uga.edu>
2016-04-22 14:16:07 -0400

    Create People.java
    another pojo

---
+++ People.java
@@ -0,0 +1,148 @@
+package model;
+
+public class People {
+	private String fName;
+	private String lName;
+	private String email;
+	private String address;
+	private int phoneNumber;
+	private int dateOfBirth;
+	private String gender;
+	private String positionTitle;
+	private int radioNumber;
+	private int stationNumber;
+	private boolean isActive;
+	/**
+	 * @return the fName
+	 */
+	public String getfName() {
+		return fName;
+	}
+	/**
+	 * @param fName the fName to set
+	 */
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	/**
+	 * @return the lName
+	 */
+	public String getlName() {
+		return lName;
+	}
+	/**
+	 * @param lName the lName to set
+	 */
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the phoneNumber
+	 */
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	/**
+	 * @return the dateOfBirth
+	 */
+	public int getDateOfBirth() {
+		return dateOfBirth;
+	}
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 */
+	public void setDateOfBirth(int dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	/**
+	 * @return the positionTitle
+	 */
+	public String getPositionTitle() {
+		return positionTitle;
+	}
+	/**
+	 * @param positionTitle the positionTitle to set
+	 */
+	public void setPositionTitle(String positionTitle) {
+		this.positionTitle = positionTitle;
+	}
+	/**
+	 * @return the radioNumber
+	 */
+	public int getRadioNumber() {
+		return radioNumber;
+	}
+	/**
+	 * @param radioNumber the radioNumber to set
+	 */
+	public void setRadioNumber(int radioNumber) {
+		this.radioNumber = radioNumber;
+	}
+	/**
+	 * @return the stationNumber
+	 */
+	public int getStationNumber() {
+		return stationNumber;
+	}
+	/**
+	 * @param stationNumber the stationNumber to set
+	 */
+	public void setStationNumber(int stationNumber) {
+		this.stationNumber = stationNumber;
+	}
+	/**
+	 * @return the isActive
+	 */
+	public boolean isActive() {
+		return isActive;
+	}
+	/**
+	 * @param isActive the isActive to set
+	 */
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+}
